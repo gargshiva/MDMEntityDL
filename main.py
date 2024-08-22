6c3d7a70ce135bd22afd20c747b57abebda9319c
silkykansal58 <SilkyKansal10@gmail.com>
2024-08-22 13:01:22 +0100

    Initial Setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from lib.logger import Log4J
+from lib.utils import Utils
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print('Usage: Runtime args are missing !')
+        sys.exit(-1)
+
+    env = sys.argv[1].upper()
+    spark = Utils.get_spark_session(env)
+    logger = Log4J(spark)
+
+    logger.info(spark.sparkContext.getConf().toDebugString())
