fbf3080e6616ec668043b2b7d3c4fa079c499710
ShivaGarg <shivagarg91@gmail.com>
2024-09-02 16:52:13 +0100

    Added Pytest

---
+++ test_pytest_config.py
@@ -0,0 +1,15 @@
+import pytest
+
+from lib.config_loader import load_app_config
+from lib.utils import Utils
+
+
+@pytest.fixture(scope='session')
+def spark():
+    return Utils.get_spark_session('LOCAL')
+
+
+def test_config_load():
+    conf = load_app_config('LOCAL')
+    print(conf)
+    assert conf['enable.hive'] == 'False'
